6b4a5aaab859d1d9074ec137867e8e844277fd41
Mike Henderyckx <mike.henderyckx@gmail.com>
2018-08-30 19:16:00 +0200

    Use mosca to set up a mqtt-broker which writes data to a local mongo database

---
+++ index.js
@@ -0,0 +1,31 @@
+var mosca = require('mosca');
+
+var server = new mosca.Server({
+  port: 1883,
+  id: 'mqtt-broker',
+  backend: {
+    type: 'mongo',
+    url: 'mongodb://localhost:27017/mqtt-broker',
+    pubsubCollection: 'messages',
+    mongo: {},
+  },
+  persistence: {
+    factory: mosca.persistence.Mongo,
+    url: "mongodb://localhost:27017/mqtt-broker",
+  }
+});
+
+
+server.on('clientConnected', function(client) {
+  console.log('CLIENT CONNECTED', client.id);
+});
+
+server.on('published', function(packet, client) {
+  if (!packet.topic.startsWith('$SYS')) {
+    console.log('Published', packet.payload);
+  }
+});
+
+server.on('ready', function setup() {
+  console.log('Mosca server is up and running');
+});
